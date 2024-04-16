c0f0a7e7183cc9278bf39ba36c31f447e3dd6464
RafidHasan121 <Rafidhasan121@gmail.com>
2024-04-16 09:52:11 +0600

    cors try

--- chatbotAPI/settings.py
+++ chatbotAPI/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'API',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
