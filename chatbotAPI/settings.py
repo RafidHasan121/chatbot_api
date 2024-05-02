5c26e7994cb4246390c03070156710a46bf43bc0
RafidHasan121 <Rafidhasan121@gmail.com>
2024-05-02 11:16:33 +0600

    daphne setup + requirements.txt update

--- chatbotAPI/settings.py
+++ chatbotAPI/settings.py
@@ -37,6 +37,7 @@ load_dotenv()
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,6 +82,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatbotAPI.wsgi.application'
+ASGI_APPLICATION = 'chatbotAPI.asgi.application'
 
 
 # Database
